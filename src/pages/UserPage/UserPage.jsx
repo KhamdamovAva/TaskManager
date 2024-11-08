b3afafbdde2e8fb047850a27badf353189325897
JamshidPts <jamshidpts14@gmail.com>
2024-11-08 15:46:15 +0500

    I added function renderContent and I used useState

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,12 +1,34 @@
 import Sidebar from '../../components/sidebar/Sidebar.jsx'
 import Navigation from '../../components/navbar/Navigation'
+import { useState } from 'react';
+import Today from '../../components/Today/Today.jsx';
+import Weekly from '../../components/Weekly/Weekly.jsx';
+import Monthly from '../../components/Monthly/Monthly.jsx';
 
 function UserPage() {
+  const [activeTab, setActiveTab] = useState('today');
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case 'today':
+        return <Today />;
+      case 'weekly':
+        return <Weekly />;
+      case 'monthly':
+        return <Monthly />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className='container w-[70%] border border-[#5200FF]'>
       <Navigation />
       <div className='flex w-full'>
-        <Sidebar />
+        <Sidebar setActiveTab={setActiveTab} />
+        <div className='content'>
+          {renderContent()}
+        </div>
       </div>
     </div>
 
