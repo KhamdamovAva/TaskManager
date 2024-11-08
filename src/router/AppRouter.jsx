dcc3347d6da4c9437d037d7cc7217f346313f540
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-08 14:32:50 +0500

    i edited ul list in Sidebar.jsx and deleted data.js

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import MainP from '../pages/mainPage/MainP'
 import Register from '../pages/registerPage/Register'
 import Login from '../pages/loginPage/Login'
 import UserPage from '../pages/UserPage/UserPage'
+import Weekly from '../pages/weeklyPage/Weekly'
 function AppRouter() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function AppRouter() {
         <Route path='/register' element={<Register />}></Route>
         <Route path='/login' element={<Login />} ></Route>
         <Route path='/profile' element={<UserPage />} ></Route>
+        <Route path='/weekly' element={<Weekly />} ></Route>
       </Routes>
     </Router>
   )
